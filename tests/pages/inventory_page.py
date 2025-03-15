44019f0fad8af979a19ba8f00f3fe3fced63c96b
yasiqb89 <mian.yassir@gmail.com>
2025-03-15 23:04:30 +0100

    Inventory page test cases: - add item - remove item - show cart

---
+++ tests/pages/inventory_page.py
@@ -0,0 +1,23 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+class InventoryPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.add_to_cart = page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]")
+        self.cart_button = page.locator("[data-test=\"shopping-cart-link\"]")
+        self.remove_from_cart = page.locator("[data-test=\"remove-sauce-labs-backpack\"]")
+
+    def add_item_to_cart(self):
+        "Adds product to cart"
+        self.add_to_cart.click()
+    
+    def remove_item_from_cart(self):
+        "Remoces a product from cart"
+        self.remove_from_cart.click()
+    
+    def open_cart(self):
+        "Click on cart button to cart page"
+        self.cart_button.click()
+    
---
+++ tests/test_inventory.py
@@ -0,0 +1,37 @@
+import pytest
+from tests.pages.login_page import LoginPage
+from tests.pages.inventory_page import InventoryPage
+from playwright.sync_api import Page, expect
+
+def test_add_item_to_cart(page: Page):
+    "Testing adding an item to the cart"
+    page.goto("https://www.saucedemo.com/")
+    login_page = LoginPage(page)
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page = InventoryPage(page)
+    inventory_page.add_item_to_cart()
+    expect(page.locator("[data-test=\"remove-sauce-labs-backpack\"]")).to_be_visible()
+
+
+def test_remove_item_from_cart(page: Page):
+    "Testing remove item from cart"
+    page.goto("https://www.saucedemo.com/")
+    login_page = LoginPage(page)
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page = InventoryPage(page)
+    inventory_page.add_item_to_cart()
+    inventory_page.remove_item_from_cart()
+    expect(page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]")).to_be_visible()
+
+def test_open_cart(page: Page):
+    """Test opening the cart page"""
+    page.goto("https://www.saucedemo.com/")
+    login_page = LoginPage(page)
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page = InventoryPage(page)
+    inventory_page.open_cart()
+
+    expect(page.locator("[data-test=\"title\"]")).to_contain_text("Your Cart")
