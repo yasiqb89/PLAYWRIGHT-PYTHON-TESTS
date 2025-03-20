d9597789e8db11e9bf99f6fda369645e583316a6
yasiqb89 <mian.yassir@gmail.com>
2025-03-20 02:05:21 +0100

    Added: parametrize test cases for checkout stage and login stage

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -4,6 +4,7 @@ from tests.pages.inventory_page import InventoryPage
 from tests.pages.checkout_page import CheckoutPage
 from playwright.sync_api import Page, expect 
 
+
 def test_complete_chekout(page:Page):
     "End to End test: Login -> Add item -> Checkout -> Order confirmation"
 
@@ -18,5 +19,45 @@ def test_complete_chekout(page:Page):
     checkout_page.proceed_to_checkout()
     checkout_page.enter_shipping_details("John", "Wick", "123")
     checkout_page.complete_checkout()
-
     expect(checkout_page.order_success_message).to_have_text("Thank you for your order!")
+
+
+@pytest.mark.parametrize("username", ["standard_user", "standard_user", "standard_user"]) # Only these "standard_username" works on the website
+def test_complete_checkout_multiple_users(page:Page, username):
+    "Testing the checkout flow with multiple users"
+
+    page.goto("https://www.saucedemo.com")
+    login_page = LoginPage(page)
+    login_page.login(username, "secret_sauce")
+
+    inventory_page = InventoryPage(page)
+    inventory_page.add_to_cart.click()
+
+    checkout_page = CheckoutPage(page, inventory_page)
+    checkout_page.proceed_to_checkout()
+    checkout_page.enter_shipping_details("John", "Wick", "123")
+    checkout_page.complete_checkout()
+    expect(checkout_page.order_success_message).to_have_text("Thank you for your order!")
+
+
+@pytest.mark.parametrize("first_name, last_name, postal_code, expected_error",[
+    ("", "Wick", "12345", "Error: First Name is required"),
+    ("John", "", "12345", "Error: Last Name is required"),
+    ("John", "Wick", "", "Error: Postal Code is required")])
+def test_checkout_with_invalid_data(page:Page, first_name, last_name, postal_code, expected_error):
+    "Test checkout stage with different scenarios: missing first name, last name and postal code"
+
+    page.goto("https://www.saucedemo.com")
+    login_page = LoginPage(page)
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page = InventoryPage(page)
+    inventory_page.add_to_cart.click()
+
+    checkout_page = CheckoutPage(page, inventory_page)
+    checkout_page.proceed_to_checkout()
+    checkout_page.enter_shipping_details(first_name, last_name, postal_code)
+
+    #Check error message
+    error_message = page.locator("[data-test='error']")
+    expect(error_message).to_have_text(expected_error)
--- tests/test_login.py
+++ tests/test_login.py
@@ -11,3 +11,16 @@ def test_valid_login(page: Page):
     expect(page.locator("[data-test=\"primary-header\"]")).to_contain_text("Swag Labs")
 
 
+@pytest.mark.parametrize("username, password, expected_error",[
+    ("", "secret_sauce", "Epic sadface: Username is required"),
+    ("standard_user", "", "Epic sadface: Password is required"),
+    ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out.")])
+def test_login_with_invalid_credentials(page:Page, username, password, expected_error):
+    "Testing login page with invalid login credentials"
+    page.goto("https://www.saucedemo.com/")
+    loging_page = LoginPage(page)
+    loging_page.login(username, password)
+
+    #Check error message
+    error_message = page.locator("[data-test='error']")
+    expect(error_message).to_have_text(expected_error)
