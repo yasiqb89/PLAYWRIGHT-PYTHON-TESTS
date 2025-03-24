eacbc9f601c7e7f68b34218c4c28753a54a78615
yasiqb89 <mian.yassir@gmail.com>
2025-03-24 02:45:27 +0100

    Test added: locked out user

--- tests/test_login.py
+++ tests/test_login.py
@@ -25,3 +25,12 @@ def test_logout_session(page: Page):
     expect(page).to_have_url("https://www.saucedemo.com/")
     
 
+def test_locked_out_user_shows_error_message(page: Page):
+    page.goto("https://www.saucedemo.com/")
+    login_page = LoginPage(page)
+
+    login_page.login("locked_out_user", "secret_sauce")
+
+    # Check for locked-out error message
+    expect(login_page.error_message).to_be_visible()
+    expect(login_page.error_message).to_have_text("Epic sadface: Sorry, this user has been locked out.")
